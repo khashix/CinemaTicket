a0317924b12d6a94f5744298c3546ef70889b59e
khashix <khashayarerashidi@gmail.com>
2020-01-04 20:30:58 +0330

    Taghirate Avvalie

--- First.c
+++ First.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#define SIZE 12
+struct Cinema
+{
+    int zarfiat;
+    int sans[SIZE];
+};
 int main()
 {
-    puts("Hello GitHub!");
+    int shomareFilm;
+    printf("\nBe CinemaTicket Khosh Amadid!\n\n");
+    printf("Baraye Reserve e Ticket Yeki Az Film Haye Dar Hale Ekrane Zir Ra Entekhab Namaeid: \n");
+    scanf("%d" , &shomareFilm);
     return 0;
 }
